240a6a8fa1f7ee7d748c14d11a3688f5128e4b49
matcha <meen.sankaew@gmail.com>
2021-12-14 16:58:43 +0700

    fix: login layout

--- src/routes/login/__layout.svelte
+++ src/routes/login/__layout.svelte
@@ -1,5 +1,5 @@
 <div class="bg-gradient flex min-h-screen font-sarabun">
-	<div class="container mx-auto bg-white shadow-lg p-10 my-auto grid grid-cols-4">
+	<div class="container w-[80vw] mx-auto bg-white shadow-lg p-10 my-auto grid grid-cols-4">
 		<div class="">
 			เข้าสู่
 			<img src="/MCV_logo_square.svg" class="h-24" alt="logo" />
